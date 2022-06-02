bfea5f0340c3ee1864e4b0f446bbff36273c4212
Carrington <mcarringtone@gmail.com>
2022-06-02 18:49:22 -0400

    category-routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,6 @@
+const { category, product } = require("../../models");
+const router = require("express").Router();
+
+router.post("/", function (req, res) {
+  res.send("POST request to homepage");
+});
